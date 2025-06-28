a6ef9f77b3388003aa4307dfea547d4157711913
Alicia Wang <156241013+a-lychee@users.noreply.github.com>
2025-06-28 15:49:46 -0400

    Comment out surprise

--- src/main.py
+++ src/main.py
@@ -1,7 +1,7 @@
 from fastapi import FastAPI, Query
 from typing import List
 from .recommend import recommend_movies
-from .generate_collab_recs import recommend_unseen_movies
+# from .generate_collab_recs import recommend_unseen_movies  # Disabled for now
 
 app = FastAPI()
 
@@ -14,29 +14,30 @@ def recommend_by_content(title: str = Query(..., description="Movie title to bas
     recs = recommend_movies(title)
     return recs['title'].tolist()
 
-@app.get("/recommend/collab", response_model=List[str])
-def recommend_by_collab(n: int = 5):
-    # Reuse the core logic but return titles only
-    import pandas as pd
-    from surprise import SVD, Dataset, Reader
+# Collaborative filtering temporarily disabled due to memory limits on Render's free tier
+# @app.get("/recommend/collab", response_model=List[str])
+# def recommend_by_collab(n: int = 5):
+#     # Reuse the core logic but return titles only
+#     import pandas as pd
+#     from surprise import SVD, Dataset, Reader
 
-    ratings_df = pd.read_csv("data/ratings.csv")
-    ratings_df['user_id'] = 1
+#     ratings_df = pd.read_csv("data/ratings.csv")
+#     ratings_df['user_id'] = 1
 
-    reader = Reader(rating_scale=(1, 5))
-    data = Dataset.load_from_df(ratings_df[['user_id', 'title', 'rating']], reader)
-    trainset = data.build_full_trainset()
-    model = SVD()
-    model.fit(trainset)
+#     reader = Reader(rating_scale=(1, 5))
+#     data = Dataset.load_from_df(ratings_df[['user_id', 'title', 'rating']], reader)
+#     trainset = data.build_full_trainset()
+#     model = SVD()
+#     model.fit(trainset)
 
-    movies_df = pd.read_csv("data/ml-latest-small/movies.csv")
-    rated = set(ratings_df['title'])
-    unseen = movies_df[~movies_df['title'].isin(rated)]
+#     movies_df = pd.read_csv("data/ml-latest-small/movies.csv")
+#     rated = set(ratings_df['title'])
+#     unseen = movies_df[~movies_df['title'].isin(rated)]
 
-    predictions = []
-    for title in unseen['title']:
-        pred = model.predict(uid=1, iid=title)
-        predictions.append((title, pred.est))
+#     predictions = []
+#     for title in unseen['title']:
+#         pred = model.predict(uid=1, iid=title)
+#         predictions.append((title, pred.est))
 
-    top_recs = sorted(predictions, key=lambda x: x[1], reverse=True)[:n]
-    return [title for title, _ in top_recs]
+#     top_recs = sorted(predictions, key=lambda x: x[1], reverse=True)[:n]
+#     return [title for title, _ in top_recs]
